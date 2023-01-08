c6525bd3785a418cc9285399234ce556ba0cf82c
Catzzz <chuneleu@gmail.com>
2023-01-08 20:38:48 +0800

    comment out public subnet b

--- deploy/network.tf
+++ deploy/network.tf
@@ -21,7 +21,10 @@ resource "aws_internet_gateway" "main" {
 
 #####################################################
 # Public Subnets - Inbound/Outbound Internet Access #
+# Subnet a
 #####################################################
+
+
 resource "aws_subnet" "public_a" {
   cidr_block              = "10.1.1.0/24"
   map_public_ip_on_launch = true
@@ -73,121 +76,63 @@ resource "aws_nat_gateway" "public_a" {
   )
 }
 
-resource "aws_subnet" "public_b" {
-  cidr_block              = "10.1.2.0/24"
-  map_public_ip_on_launch = true
-  vpc_id                  = aws_vpc.main.id
-  availability_zone       = "${data.aws_region.current.name}b"
 
-  tags = merge(
-    local.common_tags,
-    tomap({ "Name" = "${local.prefix}-public-b" })
-  )
-}
-
-resource "aws_route_table" "public_b" {
-  vpc_id = aws_vpc.main.id
-
-  tags = merge(
-    local.common_tags,
-    tomap({ "Name" = "${local.prefix}-public-b" })
-  )
-}
-
-resource "aws_route_table_association" "public_b" {
-  subnet_id      = aws_subnet.public_b.id
-  route_table_id = aws_route_table.public_b.id
-}
-
-resource "aws_route" "public_internet_access_b" {
-  route_table_id         = aws_route_table.public_b.id
-  destination_cidr_block = "0.0.0.0/0"
-  gateway_id             = aws_internet_gateway.main.id
-}
-
-resource "aws_eip" "public_b" {
-  vpc = true
-
-  tags = merge(
-    local.common_tags,
-    tomap({ "Name" = "${local.prefix}-public-b" })
-  )
-}
-
-resource "aws_nat_gateway" "public_b" {
-  allocation_id = aws_eip.public_b.id
-  subnet_id     = aws_subnet.public_b.id
-
-  tags = merge(
-    local.common_tags,
-    tomap({ "Name" = "${local.prefix}-public-b" })
-  )
-}
+#####################################################
+# Public Subnets - Subnet b (optional) #
+#####################################################
 
 
-# resource "aws_vpc" "main" {
-#   cidr_block           = "10.1.0.0/16"
-#   enable_dns_support   = true
-#   enable_dns_hostnames = true
 
-#   tags = merge(
-#     local.common_tags,
-#     tomap({ "Name" = "${local.prefix}-vpc" })
-#   )
-# }
-
-# resource "aws_internet_gateway" "main" {
-#   vpc_id = aws_vpc.main.id
-
-#   tags = merge(
-#     local.common_tags,
-#     tomap({ "Name" = "${local.prefix}-main" })
-#   )
-# }
-
-
-# #####################################################
-# # Public Subnets - Inbound/Outbound Internet Access #
-# #####################################################
-# resource "aws_subnet" "public_a" {
-#   cidr_block              = "10.1.1.0/24"
+# resource "aws_subnet" "public_b" {
+#   cidr_block              = "10.1.2.0/24"
 #   map_public_ip_on_launch = true
 #   vpc_id                  = aws_vpc.main.id
-#   availability_zone       = "${data.aws_region.current.name}a"
+#   availability_zone       = "${data.aws_region.current.name}b"
 
 #   tags = merge(
 #     local.common_tags,
-#     tomap({ "Name" = "${local.prefix}-public-a" })
+#     tomap({ "Name" = "${local.prefix}-public-b" })
 #   )
 # }
 
-# resource "aws_route_table" "public_a" {
+# resource "aws_route_table" "public_b" {
 #   vpc_id = aws_vpc.main.id
 
 #   tags = merge(
 #     local.common_tags,
-#     tomap({ "Name" = "${local.prefix}-public-a" })
+#     tomap({ "Name" = "${local.prefix}-public-b" })
 #   )
 # }
 
-# resource "aws_route_table_association" "public_a" {
-#   subnet_id      = aws_subnet.public_a.id
-#   route_table_id = aws_route_table.public_a.id
+# resource "aws_route_table_association" "public_b" {
+#   subnet_id      = aws_subnet.public_b.id
+#   route_table_id = aws_route_table.public_b.id
 # }
 
-# resource "aws_route" "public_internet_access_a" {
-#   route_table_id         = aws_route_table.public_a.id
+# resource "aws_route" "public_internet_access_b" {
+#   route_table_id         = aws_route_table.public_b.id
 #   destination_cidr_block = "0.0.0.0/0"
 #   gateway_id             = aws_internet_gateway.main.id
 # }
 
-# resource "aws_eip" "public_a" {
+# resource "aws_eip" "public_b" {
 #   vpc = true
 
 #   tags = merge(
 #     local.common_tags,
-#     tomap({ "Name" = "${local.prefix}-public-a" })
+#     tomap({ "Name" = "${local.prefix}-public-b" })
 #   )
 # }
+
+# resource "aws_nat_gateway" "public_b" {
+#   allocation_id = aws_eip.public_b.id
+#   subnet_id     = aws_subnet.public_b.id
+
+#   tags = merge(
+#     local.common_tags,
+#     tomap({ "Name" = "${local.prefix}-public-b" })
+#   )
+# }
+
 
 
