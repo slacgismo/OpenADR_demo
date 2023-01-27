130f9bd84c26af80cfed226f8fbef1aa5b898f2d
Catzzz <chuneleu@gmail.com>
2023-01-26 21:22:11 -0800

    update user-data

--- deploy/templates/vtn/vtn-user-data.sh
+++ deploy/templates/vtn/vtn-user-data.sh
@@ -17,14 +17,19 @@ sudo docker-compose version
 echo "mkdir home/ec2-user/openADR"
 sudo chmod +rwx home/ec2-user/openADR
 sudo git clone https://github.com/slacgismo/OpenADR_demo.git /home/ec2-user/openADR
-echo "start docker-compose"
+
 # start docker-compose
-cd /home/ec2-user/openADR/docker-vtn
-docker-compose up 
+
+
 touch  /home/ec2-user/openADR/tmp.txt
 chmod +rwx /home/ec2-user/openADR/tmp.txt
-
-echo "export DB_HOST=${DB_HOST}" | sudo tee -a /home/ec2-user/openADR/tmp.txt #  -a for append (>>)
+echo "============================"
+echo "export DB_HOST=${DB_HOST}" | sudo tee -a /home/ec2-user/openADR/tmp.txt 
 echo "export DB_USER=${DB_USER}" | sudo tee -a /home/ec2-user/openADR/tmp.txt
 echo "export DB_PASSWORD=${DB_PASSWORD}" | sudo tee -a /home/ec2-user/openADR/tmp.txt
 echo "export DB_NAME=${DB_NAME}" | sudo tee -a /home/ec2-user/openADR/tmp.txt
+source .tmp.txt
+echo "============================"
+echo "start docker-compose"
+cd /home/ec2-user/openADR/docker-vtn
+docker-compose up 
