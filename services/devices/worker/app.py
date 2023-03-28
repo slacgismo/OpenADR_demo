349ca30deec9b9f74599ae783f6cd94407e28f1b
Catzzz <chuneleu@gmail.com>
2023-03-28 08:49:32 -0700

    fix the os.environ.get() and replace aws_region with AWS_DEFAULT_REGION. fix typo ACKEND_S3_BUCKET_NAME

--- services/devices/worker/app.py
+++ services/devices/worker/app.py
@@ -21,46 +21,46 @@ from process_acitons.process_task_from_fifo_sqs import process_task_from_fifo_sq
 logging.basicConfig(
     format="%(asctime)s %(message)s", datefmt="%m/%d/%Y %I:%M:%S %p", level=logging.INFO
 )
-
-SQS_GROUPID = os.environ["SQS_GROUPID"]
+SQS_GROUPID = os.environ.get("SQS_GROUPID")
 if SQS_GROUPID is None:
     raise Exception("SQS_GROUPID is not set")
 logging.info(f"sqs group id is {SQS_GROUPID}")
 
-ENV = os.environ["ENV"]
+ENV = os.environ.get("ENV")
 if ENV is None:
     raise Exception("ENV is not set")
 logging.info(f"ENV  is {ENV}")
-WORKER_PORT = os.environ["WORKER_PORT"]
+WORKER_PORT = os.environ.get("WORKER_PORT")
 if WORKER_PORT is None:
     raise Exception("WORKER_PORT not set")
 logging.info(f"worker port is {WORKER_PORT}")
 
-FIFO_SQS_URL = os.environ["FIFO_SQS_URL"]
+FIFO_SQS_URL = os.environ.get("FIFO_SQS_URL")
 if FIFO_SQS_URL is None:
     raise Exception("FIFO_SQS_URL is not set")
 
 
-BACKEND_S3_BUCKET_NAME = os.environ["BACKEND_S3_BUCKET_NAME"]
+BACKEND_S3_BUCKET_NAME = os.environ.get("BACKEND_S3_BUCKET_NAME")
 if BACKEND_S3_BUCKET_NAME is None:
     raise Exception("BACKEND_S3_BUCKET_NAME is not set")
 
 
-FIFO_DLQ_URL = os.environ["FIFO_DLQ_URL"]
+FIFO_DLQ_URL = os.environ.get("FIFO_DLQ_URL")
 if FIFO_DLQ_URL is None:
     raise Exception("FIFO_DLQ_URL is not set")
 
-AWS_REGION = os.environ["AWS_REGION"]
-if AWS_REGION is None:
+AWS_DEFAULT_REGION = os.environ.get("AWS_DEFAULT_REGION")
+if AWS_DEFAULT_REGION is None:
     raise Exception("AWS_REGION is not set")
 
-DYNAMODB_AGENTS_SHARED_REMOTE_STATE_LOCK_TABLE_NAME = os.environ[
+DYNAMODB_AGENTS_SHARED_REMOTE_STATE_LOCK_TABLE_NAME = os.environ.get(
     "DYNAMODB_AGENTS_SHARED_REMOTE_STATE_LOCK_TABLE_NAME"
-]
+)
 if DYNAMODB_AGENTS_SHARED_REMOTE_STATE_LOCK_TABLE_NAME is None:
-    raise Exception("DYNAMODB_AGENTS_SHARED_REMOTE_STATE_LOCK_TABLE_NAME is not set")
+    raise Exception(
+        "DYNAMODB_AGENTS_SHARED_REMOTE_STATE_LOCK_TABLE_NAME is not set")
 
-ECS_CLUSTER_NAME = os.environ["ECS_CLUSTER_NAME"]
+ECS_CLUSTER_NAME = os.environ.get("ECS_CLUSTER_NAME")
 if ECS_CLUSTER_NAME is None:
     raise Exception("ECS_CLUSTER_NAME is not set")
 
@@ -85,7 +85,7 @@ def main():
             FIFO_SQS_URL,
             BACKEND_S3_BUCKET_NAME,
             DYNAMODB_AGENTS_SHARED_REMOTE_STATE_LOCK_TABLE_NAME,
-            AWS_REGION,
+            AWS_DEFAULT_REGION,
             ENV,
             SQS_GROUPID,
         ),
